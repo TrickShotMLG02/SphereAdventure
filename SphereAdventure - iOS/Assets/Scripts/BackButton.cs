e6ada77392c54288a1ac4daaa04d55c50277681d
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2018-11-04 20:12:52 +0100

    SphereAdventure-v1.4

---
+++ SphereAdventure - iOS/Assets/Scripts/BackButton.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;  
+    
+	
+public class BackButton : MonoBehaviour {
+	
+	
+	public GameObject PauseMenu;
+	public GameObject SettingsMenu;
+	public GameObject PauseMenuToDisable;
+	public GameObject SettingsMenuToDisable;
+	public GameObject Button;
+	public GameObject Button2;
+	
+	public bool toggleBool = false;
+	public bool toggleBool2 = false;
+	
+	
+    void Start() {
+		
+		//PauseMenu.SetActive(false);
+		
+        
+    }
+        public void TogglePause1() {      
+			
+			toggleBool = !toggleBool;
+			SettingsMenu.SetActive(toggleBool);
+			
+			PauseMenuToDisable.SetActive(!toggleBool);
+			
+			toggleBool = !toggleBool;
+    }
+	
+	
+	 public void TogglePause2() {      
+			
+			toggleBool2 = !toggleBool2;
+			Debug.Log("Pausemenu" + toggleBool2);
+			PauseMenu.SetActive(toggleBool2);
+			
+			Debug.Log("Settingsmenu" + !toggleBool2);
+			SettingsMenuToDisable.SetActive(!toggleBool2);
+			
+			toggleBool2 = false;
+    }
+	
+		void Update ()
+		{
+			Button.GetComponent<Button>().onClick.AddListener(TogglePause1);
+			Button2.GetComponent<Button>().onClick.AddListener(TogglePause2);
+		}	
+}
---
+++ SphereAdventure - iOS/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;  
+    
+	
+public class PauseButton : MonoBehaviour {
+	
+	
+	public float SkinNumber;
+	public GameObject Player1;
+	public GameObject Player2;
+	public GameObject Player3;
+	
+	public GameObject ScoreDisplay;
+	public GameObject PauseMenu;
+	public GameObject PButton;
+	public GameObject Map;
+	
+	public float Skinnumber;
+	
+	public bool toggleBool = false;
+	
+	public float GameSpeed;
+	
+    void Start() {
+		
+		Skinnumber = PlayerPrefs.GetFloat("SkinSelected");
+		
+		PauseMenu.SetActive(false);
+		
+        GetComponent<Button>().onClick.AddListener(TogglePause);
+    }
+	
+        public void TogglePause() {      
+			
+			toggleBool = !toggleBool;
+			
+			Map.SetActive(!toggleBool);
+			
+			
+		if(Skinnumber == 1)
+		{
+			Player1.SetActive(!toggleBool);
+		}	
+		if(Skinnumber == 2)
+		{
+			Player2.SetActive(!toggleBool);
+		}
+		if(Skinnumber == 3)
+		{
+			Player3.SetActive(!toggleBool);
+		}			
+			
+			PauseMenu.SetActive(toggleBool);
+			ScoreDisplay.SetActive(!toggleBool);		
+			
+    }
+	
+		void Update ()
+		{
+			GameSpeed = PlayerPrefs.GetFloat("GameSpeed");
+		}	
+}
